147a7d846052e06b4be414874394b5bf4db90c82
Andrik Kleine <andrik.k@web.de>
2024-08-18 18:08:27 +0200

    Waagenbalken korrekt adjusted

--- Assets/Scripts/BalkenBieger.cs
+++ Assets/Scripts/BalkenBieger.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BalkenBieger : MonoBehaviour
 {
+    public ScaleWeight leftScale;
+    public ScaleWeight rightScale;
+
+    private float horizontalScaleDistance;
+    private float initialXScale;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialXScale = transform.localScale.x;
+        horizontalScaleDistance = rightScale.transform.position.x - leftScale.transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float totalScaleDistance = (rightScale.transform.position - leftScale.transform.position).magnitude;
+        transform.localScale = new Vector3(initialXScale * (totalScaleDistance / horizontalScaleDistance), transform.localScale.y, transform.localScale.z);
+
+        float angle = Mathf.Acos(horizontalScaleDistance / totalScaleDistance) * Mathf.Rad2Deg;
+        bool rightScaleHigher = rightScale.transform.position.y > leftScale.transform.position.y;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, rightScaleHigher ? angle : -angle));
     }
 }
