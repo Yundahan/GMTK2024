7b9898ee5f6b5c02fc377352f87408bb18d3b54f
Suritama <ichbindeppert@outlook.com>
2024-08-18 23:30:27 +0200

    minor music fixes, fuck grayscale

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -55,11 +55,10 @@ public class BlockMovement : MonoBehaviour
 
         if(!isBeingDragged && isOutsideOfArea)
         {
-            float h, s, v;
-            Color.RGBToHSV(GetComponent<SpriteRenderer>().color, out h, out s, out v);
+            /*Color.RGBToHSV(GetComponent<SpriteRenderer>().color, out h, out s, out v);
             Debug.Log("H: " + h + " S: " + s + " V: " + v);
-            Color newColor = Color.HSVToRGB(h, 0, v);
-            GetComponent<SpriteRenderer>().color = newColor;
+            Color newColor = Color.HSVToRGB(h, 0, v);*/
+            GetComponent<SpriteRenderer>().material.SetFloat("_GrayscaleAmount", 1);
         } else
         {
             GetComponent<SpriteRenderer>().color = initColor;
